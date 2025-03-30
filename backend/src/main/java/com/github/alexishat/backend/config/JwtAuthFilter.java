a3a8162020cea428fec279e02d409320d0eccd27
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-03-31 01:44:27 +0200

    JWT filter zwischengeschaltet. Token wird an Auth Provider weitergegeben. Dieser muss nachher implementiert werden

---
+++ backend/src/main/java/com/github/alexishat/backend/config/JwtAuthFilter.java
@@ -0,0 +1,36 @@
+package com.github.alexishat.backend.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if(header != null) {
+            String[] tokens = header.split(" ");
+            if(tokens.length == 2 && "Bearer".equals(tokens[0])) {
+                try{
+                    SecurityContextHolder.getContext().setAuthentication(
+                            userAuthProvider.validateToken(tokens[1])
+                    );
+                } catch (RuntimeException e) {
+                    SecurityContextHolder.clearContext();
+                    throw e;
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}

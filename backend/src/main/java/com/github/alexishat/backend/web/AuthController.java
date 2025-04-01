a7e1358f7cf102c66147394ee293996b94625ca4
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-04-01 13:39:09 +0200

    RestAPI endpunkt für authentifizierung von Usern erstellt

---
+++ backend/src/main/java/com/github/alexishat/backend/web/AuthController.java
@@ -0,0 +1,45 @@
+package com.github.alexishat.backend.web;
+
+import com.github.alexishat.backend.config.JwtUtil;
+import com.github.alexishat.backend.dtos.LoginDto;
+import com.github.alexishat.backend.dtos.RegisterDto;
+import com.github.alexishat.backend.dtos.UserDto;
+import com.github.alexishat.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+    private final JwtUtil tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        UserDto userDto = userService.login(loginDto);
+        return ResponseEntity.ok(tokenService.generateToken(userDto.getUsername()));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+
+            UserDto userDto = userService.register(registerDto);
+            String token = tokenService.generateToken(userDto.getUsername());
+
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(userDto.getId())
+                    .toUri();
+
+            return ResponseEntity.created(location).body(token);
+    }
+}

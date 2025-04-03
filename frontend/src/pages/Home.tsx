f919f0ef135254576bf94114c553c8fb2228ab82
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-04-03 22:35:47 +0200

    LogoutButton Component erstellt

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
+import LogoutButton from "../components/LogoutButton"; // Pfad ggf. anpassen
+import { isLoggedIn } from "../utils/auth";
 
 function Home() {
   return (
-    <>
-      <h1>Willkommen zum Study-Tracker</h1>
-      <Link to="/login">Zum Login</Link>
-    </>
+    <div className="container mt-5">
+      <h1 className="mb-4">Willkommen zum Study-Tracker</h1>
+
+      <div className="mb-3">
+        <Link to="/login" className="btn btn-primary me-3">
+          Zum Login
+        </Link>
+        {isLoggedIn() && <LogoutButton />}
+      </div>
+    </div>
   );
 }
 

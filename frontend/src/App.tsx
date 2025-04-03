45431f10d99bc7fd8d37e0fb1a9add78ba727578
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-04-03 22:39:22 +0200

    fixed "bug" wo noch About als component angegeben war

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/Login";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );

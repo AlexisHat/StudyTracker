9d089008fab72092ec372f0b7d8126dbdf8c0b05
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-04-05 18:22:56 +0200

    Methode zum senden der erstellten Sessions erstellt

--- frontend/src/service/TopicService.ts
+++ frontend/src/service/TopicService.ts
@@ -1,6 +1,21 @@
 import { protectedApi } from "../api/api"
+import { CreateSessionData } from "../types/session.types";
 
 export const getTopics = async (): Promise<string[]> =>{
     const response = await protectedApi.get("/topics");
   return response.data;
 };
+
+export const sendSession = async (data: CreateSessionData) => {
+    try {
+        const response = await protectedApi.post("/sessions/create", data);
+    
+        if (response.status === 201) {
+          return response.data;
+        } else {
+          console.error("Fehler: Unerwarteter Statuscode", response.status);
+        }
+      } catch (error) {
+        console.error("Fehler beim Senden der Session:", error);
+      }
+}

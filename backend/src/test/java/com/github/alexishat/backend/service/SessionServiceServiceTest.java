8d8e29996b0b2b959ab3c18ff0a4e93ab680da25
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-04-07 23:28:34 +0200

    Tests zu Session Service angefangen

---
+++ backend/src/test/java/com/github/alexishat/backend/service/SessionServiceServiceTest.java
@@ -0,0 +1,29 @@
+package com.github.alexishat.backend.service;
+
+import com.github.alexishat.backend.dtos.SessionDto;
+import com.github.alexishat.backend.model.Session;
+import com.github.alexishat.backend.model.Topic;
+import com.github.alexishat.backend.model.User;
+import com.github.alexishat.backend.repositories.SessionRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class SessionServiceServiceTest{
+
+
+    @Test
+    @DisplayName("create ruft das repo zum speichern aus und gibt eine gespeicherte zurück")
+    void test_01(){
+        SessionRepository sessionRepository = mock(SessionRepository.class);
+        UserService userService= mock(UserService.class);
+        TopicService topicService = mock(TopicService.class);
+
+        SessionService service = new SessionService(sessionRepository, userService, topicService);
+        Session created = service.create(SessionDto.builder().build(), new User(), new Topic());
+
+        verify(sessionRepository, times(1)).save(any());
+    }
+}

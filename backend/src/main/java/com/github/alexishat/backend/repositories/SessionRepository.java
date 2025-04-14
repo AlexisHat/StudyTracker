875901ba992e26f2f5ec42665aa863f091711f6b
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-04-14 10:08:20 +0200

    Repo Methode um alle Sessions für einen bestimmten Tag zu bekommen

--- backend/src/main/java/com/github/alexishat/backend/repositories/SessionRepository.java
+++ backend/src/main/java/com/github/alexishat/backend/repositories/SessionRepository.java
@@ -6,9 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Integer> {
     @Query("SELECT s FROM Session s WHERE s.user = :user AND YEAR(s.startzeit) = :year")
     List<Session> findAllByUserAndYear(@Param("user") User user, @Param("year") int year);
+
+    @Query("SELECT s FROM Session s WHERE s.user = :user AND s.endzeit >= :startOfDay AND s.endzeit < :startOfNextDay")
+    List<Session> findAllByUserAndDay(@Param("user") User user,
+                                               @Param("startOfDay") LocalDateTime startOfDay,
+                                               @Param("startOfNextDay") LocalDateTime startOfNextDay);
+
 }
